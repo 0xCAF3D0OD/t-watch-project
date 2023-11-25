e90cb5c9f8069be2866de430894ee4d6b24dd068
Bastien Schwitz <bschwitz33@gmail.com>
2023-11-25 20:00:35 +0100

    class

--- src/main.hpp
+++ src/main.hpp
@@ -4,10 +4,13 @@
 #include <Arduino.h>
 #include <TWatch_hal.h>
 #include <iostream>
+#include <string>
 //"TWatch_config.h" needs to be configured in the library for the first time
 // This routine needs to annotate the LVGL GUI
 
 #include "backend/backend.hpp"
+
+using namespace std;
 
 void	setup();
 void	loop();
