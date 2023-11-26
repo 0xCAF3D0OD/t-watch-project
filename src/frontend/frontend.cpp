be7528f3e7619f83276acac6e1dca7c6f70d9d25
dino <kevin.dinocera@protonmail.com>
2023-11-26 10:56:35 +0100

    rm comments

--- src/frontend/frontend.cpp
+++ src/frontend/frontend.cpp
@@ -11,4 +11,3 @@ void lv_example_get_started_1(void)
     lv_obj_set_style_text_color(lv_scr_act(), lv_color_hex(0xffffff), LV_PART_MAIN);
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
 }
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "main.hpp"
-
 TWatchClass *twatch = nullptr;
 TFT_eSPI *tft = nullptr;
 
@@ -63,56 +62,5 @@ void setup() {
 }
 
 void loop() {
-  // if (xEventGroupGetBits(twatch->_Hal_IRQ_event) & TOUCH_IRQ_BIT) {
-  //   xEventGroupClearBits(twatch->_Hal_IRQ_event, TOUCH_IRQ_BIT);
-  //   tft->drawString(buf, 80, 118);
-  // }
   delay(20);
 }
-
-// //"TWatch_config.h" needs to be configured in the library for the first time
-// // This routine needs to annotate the LVGL GUI
-
-// TWatchClass *twatch = nullptr;
-// TFT_eSPI *tft = nullptr;
-
-// void btn1_click(void *param) {
-//   Serial.println("BTN1 Click");
-//   tft->drawString("BTN1 Click", 80, 118);
-// }
-// void btn2_click(void *param) {
-//   Serial.println("BTN2 Click");
-//   tft->drawString("BTN2 Click", 80, 118);
-// }
-// void btn3_click(void *param) {
-//   Serial.println("BTN3 Click");
-//   tft->drawString("BTN3 Click", 80, 118);
-// }
-
-// void setup() {
-//   twatch = TWatchClass::getWatch();
-//   tft = twatch->tft_get_instance();
-
-//   Serial.begin(115200);
-//   Serial.println("Button Demo (TFT_eSPI)");
-
-//   twatch->motor_shake(2, 50);
-
-//   tft->fillScreen(TFT_BLACK);
-//   twatch->backlight_set_value(255);
-
-//   twatch->hal_auto_update(true, 0);
-
-//   tft->setTextFont(2);
-//   tft->setTextColor(TFT_BLACK, TFT_WHITE);
-//   tft->drawString("T-Watch Button Test", 62, 90);
-
-//   twatch->button_bind_event(TWATCH_BTN_1, BUTTON_CLICK, btn1_click);
-//   twatch->button_bind_event(TWATCH_BTN_2, BUTTON_CLICK, btn2_click);
-//   twatch->button_bind_event(TWATCH_BTN_3, BUTTON_CLICK, btn3_click);
-// }
-
-// void loop() {
-//   // put your main code here, to run repeatedly:
-// }
-
